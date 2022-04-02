93c11048b35d25938b70c8079a4d4c4f40e8bdd7
Harshit Panwar <harshitpanwar234567@gmail.com>
2022-04-02 14:35:28 +0530

    connection to database done

--- server/config/database.js
+++ server/config/database.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongo://localhost:27017/Ecommerce',)
+const connectDatabase = ()=>{
+
+mongoose.connect(process.env.DB_URI,{useUnifiedTopology:true})
+.then(
+(data)=>{
+    console.log(`Mongodb connected with server: ${data.connection.host}`);
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+module.exports = connectDatabase
