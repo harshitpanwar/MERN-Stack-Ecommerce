ca4aa2e44b74139f2d41c3e0baa84400fa6353f1
Harshit Panwar <harshitpanwar234567@gmail.com>
2022-04-03 02:17:39 +0530

    uncaught excep. and unhandled pr. rej. done

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const dotenv = require('dotenv');
 const connectDatabase = require("./config/database");
 
 
+//Handling Uncaught Exception
+process.on("uncaughtException",(err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+})
+
 // config 
 
 dotenv.config({path:"server/config/config.env"});
@@ -18,4 +25,15 @@ app.listen(process.env.PORT,() => {
 
     console.log(`process in running on port ${process.env.PORT}`)
 
+});
+
+
+//Unhandeled Promise Rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhadeled Promise Rejection`);
+
+    server.close(()=>{
+        process.exit(1);
+    })
 })
